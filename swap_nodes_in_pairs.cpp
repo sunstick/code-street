3d8abc5f04309cede9dcb6a56082acb98ff8e7e3
sunstick <sunstick@gmail.com>
2012-09-24 00:38:35 -0500

    add swap nodes in pairs

---
+++ swap_nodes_in_pairs.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *swapPairs(ListNode *head) {
+        ListNode dummy1(0);
+        ListNode dummy2(0);
+        ListNode *tail1 = &dummy1;
+        ListNode *tail2 = &dummy2;
+
+        ListNode *node = head;
+
+        // alternating split
+        int i = 0;
+        while (node) {
+            ListNode **tail;
+            if (i % 2 == 0)
+                tail = &tail1;
+            else
+                tail = &tail2;
+
+            (*tail) -> next = node;
+            node = node -> next;
+            *tail = (*tail) -> next;
+            (*tail) -> next = NULL;
+            i++;
+        }
+
+        // alternating merge
+        ListNode dummy(0);
+        ListNode *tail = &dummy;
+        ListNode *node1 = dummy1.next;
+        ListNode *node2 = dummy2.next;
+
+        while (node2 && node1) {
+            tail -> next = node2;
+            node2 = node2 -> next;
+            tail = tail -> next;
+            tail -> next = NULL;
+
+            tail -> next = node1;
+            node1 = node1 -> next;
+            tail = tail -> next;
+            tail -> next = NULL;
+        }
+
+        if (node1)
+            tail -> next = node1;
+        if (node2)
+            tail -> next = node2;
+
+        return dummy.next;
+    }
+};
